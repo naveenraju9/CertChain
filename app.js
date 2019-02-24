9f0988020111c2014abaf01d7436eef01f3c9eb1
Naveen Raju Mudhunuri <yournaveen@outlook.com>
2019-02-25 01:49:19 +0530

    added routing to static files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const CertNode = require('./CertNode'); 
-
+const path = require('path');
 const port = 18070+Math.floor(Math.random()*30);
 console.log('starting node on ', port)
 let node1 = new CertNode(port);
@@ -12,7 +12,7 @@ const http_port = 3000+Math.floor(Math.random()*10);
 
 let BrewHTTP = function (){
 	const app = new express();
-
+	app.use(express.static( 'public'));
 	app.use(bodyParser.json());
 
 	app.get('/addNode/:port', (req, res)=>{
@@ -22,12 +22,15 @@ let BrewHTTP = function (){
 		res.send();
 	})
 
-	app.get('/spawnBrew/:teammember', (req, res)=>{
+	app.get('/addData/:teammember', (req, res)=>{
 		let newBlock = node1.createBlock(req.params.teammember);
 		console.log('block created');
 		res.send();
 	})
-
+	app.get('/', function(req, res) {
+		res.sendFile(path.join(__dirname + '/index.html'));
+	});
+	
 	app.listen(http_port, () => {
 		console.log(`http server up.. ${http_port}`);
 	})
