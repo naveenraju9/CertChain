22ecb6d2b6754546b85b113f995c4f73198a5ec1
Naveen Raju Mudhunuri <yournaveen@outlook.com>
2019-02-26 09:33:27 +0530

    mongoose schema for blockchain

--- model/schema.js
+++ model/schema.js
@@ -1,16 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema();
+
+
+//Individual Subjects schema
+let marksSchema = new Schema({
+    subject: {type: String},
+    marks: {type: Number}
+});
+
+//Schema for individual Certificate
 let CertificateSchema = new Schema({
     name: {type:String},
     Dob: {type:String},
-    marks: [],
+    marks: [marksSchema],
     Hash: {type:String},
     previousHash: {type: String}
 });
+
+//Schema for the block
 let BlockchainSchema = new Schema({
     timestamp: {type:Number, required:true},
-    data: {
-    },
+    data: [CertificateSchema],
     index: {type:Number},
     previousHash: {type:String},
     nounce: {type:Number},
